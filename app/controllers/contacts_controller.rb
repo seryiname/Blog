a9cf2b620dc3c7530c9992cb77c9527ad309fc52
seryiname <slv0978563858@gmail.com>
2020-12-23 19:12:10 +0200

    Blog

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,9 @@
+class ContactsController < ApplicationController
+
+    def gmail
+        @secondcontact = 'gmail'
+
+        @adress = 'seryiname@gmail.com'
+    end
+
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+    def about
+        @heading = 'Pages_01'
+    end
+end
